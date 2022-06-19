3a94f2397f3cdd645dccbc16d27dac40f7cd0522
Andrew Brown <andy@andygbrown.co.uk>
2022-06-19 21:26:28 +0100

    basic tests with parameterised value on component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,12 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const msg = `${Date.now()}`;
+  const props = {
+    msg
+  };
+  render(<App {...props} />);
+  const rgx = new RegExp(msg, 'i');
+  const linkElement = screen.getByText(rgx);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App msg="hello world" />
   </React.StrictMode>
 );
 
