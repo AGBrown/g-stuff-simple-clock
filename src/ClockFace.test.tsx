928424fd4c7a3defb1d1774f2e984fb7c9a51b8d
Andrew Brown <andy@andygbrown.co.uk>
2022-06-19 22:41:35 +0100

    refac: loop hand divs

--- src/ClockFace.test.tsx
+++ src/ClockFace.test.tsx
@@ -11,3 +11,23 @@ test('renders minute marks', () => {
     expect(e).toBeInTheDocument();
   });
 });
+
+test('renders hand faces', () => {
+  const props = {
+  };
+  render(<ClockFace {...props} />);
+  ["second", "min", "hour"].forEach(x => {
+    const e = screen.getByText((_, e) => new RegExp(`${x}-face`).test(e?.getAttribute('class') ?? ""));
+    expect(e).toBeInTheDocument();
+  });
+});
+
+test('renders hands', () => {
+  const props = {
+  };
+  render(<ClockFace {...props} />);
+  ["second", "min", "hour"].forEach(x => {
+    const e = screen.getByText((_, e) => new RegExp(`${x}-hand`).test(e?.getAttribute('class') ?? ""));
+    expect(e).toBeInTheDocument();
+  });
+});
--- src/ClockFace.tsx
+++ src/ClockFace.tsx
@@ -6,29 +6,26 @@ interface IClockFaceProps {
 }
 
 function ClockFace(props: IClockFaceProps) {
+  var hands = ["second", "min", "hour"];
   return (
     <div className="clock">
     <div className="clock-face">
       <div className="minutes">
         {[...Array(60)].map((_, i) =>
-          <div className="tick-radius" data-minute={i} >
+          <div key={`${i}`} className="tick-radius" data-minute={i} >
             <div className="tickmark" />
           </div>
-          )}
+        )}
       </div>
       <div className="date-container">
         <p className="date">JUL 31</p>
       </div>
 
-      <div className="hand-container min-face">
-        <div className="hand min-hand"></div>
-      </div>
-      <div className="hand-container hour-face">
-        <div className="hand hour-hand"></div>
-      </div>
-      <div className="hand-container second-face">
-        <div className="hand second-hand"></div>
-      </div>
+      {[hands.map(x =>
+        <div key={x} className={["hand-container", `${x}-face`].join(' ')}>
+          <div className={["hand", `${x}-hand`].join(' ')}></div>
+        </div>
+      )]}
       <div className="center-peg"></div>
     </div>
     </div>
