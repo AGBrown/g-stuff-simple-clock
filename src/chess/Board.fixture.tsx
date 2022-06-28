790da7768a97690ef3630d64260068ce820f9bfb
Andrew Brown <andy@andygbrown.co.uk>
2022-06-28 20:23:49 +0100

    Give board a bit of layout, add multifixtures for Board

--- src/chess/Board.fixture.tsx
+++ src/chess/Board.fixture.tsx
@@ -1,11 +1,15 @@
 import Board from "./Board";
 
-function BoardFactory() {
-  return <Board />;
+function BoardFactory(x?: number, y?: number) {
+  var kp = [x === undefined ? 0 : x, y === undefined ? 0 : y] as [number, number];
+  return <Board knightPosition={kp} />;
 }
 
 const variations = {
-  simple: () => BoardFactory(),
+  simple: () => BoardFactory(0, 0),
+  simple0: () => BoardFactory(0, 0),
+  simple1: () => BoardFactory(1, 0),
+  simple2: () => BoardFactory(2, 0),
 }
 
 export default variations;
--- src/chess/Board.tsx
+++ src/chess/Board.tsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import Square from "./Square";
 import Knight from "./Knight";
+import './Board.css';
 
 export type KnightPosition = [x: number, y: number];
 export type BoardProps = {
   knightPosition?: KnightPosition
 }
 
+function renderSquare(x: number, y: number, [knightX, knightY]: KnightPosition) {
+  const black = (x + y) % 2 === 1;
+  const isKnightHere = knightX === x && knightY === y;
+  const piece = isKnightHere ? <Knight /> : null;
+
+  return <Square black={black}>{piece}</Square>
+}
+
 function Board({ knightPosition }: BoardProps) {
+  const kp = knightPosition || [1,0];
   return (
-    <div>
-      <Square black>
-        <Knight />
-      </Square>
+    <div className="Board">
+      {renderSquare(0, 0, kp)}
+      {renderSquare(1, 0, kp)}
+      {renderSquare(2, 0, kp)}
     </div>
   )
 }
