3525fe53de4c498eb9038e1b56f50ecb6c7c22dc
Andrew Brown <andy@andygbrown.co.uk>
2022-06-28 16:46:37 +0100

    Editable props on fixture

--- src/cosmos-test/HelloCosmos.fixture.tsx
+++ src/cosmos-test/HelloCosmos.fixture.tsx
@@ -1,5 +1,11 @@
 // Hello.fixture.jsx
-import React from 'react';
+import { useValue } from 'react-cosmos/fixture';
 import HelloCosmos from './HelloCosmos';
 
-export default <HelloCosmos greeting="Aloha" name="Bobby" />;
+function Fixture() {
+  const [greeting] = useValue('greeting', { defaultValue: "Aloha" } );
+  const [name] = useValue('name', { defaultValue: "x" } );
+  return <HelloCosmos greeting={greeting} name={name} />;
+}
+
+export default Fixture;
