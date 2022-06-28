d25ba7bae4d8551cc74625c9fc412c178c94ecfb
Andrew Brown <andy@andygbrown.co.uk>
2022-06-28 22:36:07 +0100

    refac: styles

--- src/chess/Board.tsx
+++ src/chess/Board.tsx
@@ -18,14 +18,8 @@ function renderSquare(i: number, ki: number, setKi: (i: number) => void) {
   const piece = isKnightHere ? <Knight /> : null;
 
   return (
-    <div key={i} style={{ position: 'relative', height: '0', width: '12.5%', paddingBottom: '12.5%' }}>
-      <div style={{
-        position: 'absolute',
-        top: '0',
-        right: '0',
-        bottom: '0',
-        left: '0',
-        height: '100%', width: '100%' }}
+    <div key={i} className="board-square-container">
+      <div className="board-droptarget"
         onClick={() => setKi(i)}>
         <Square black={black}>{piece}</Square>
       </div>
--- src/chess/Square.tsx
+++ src/chess/Square.tsx
@@ -2,20 +2,8 @@ import React from 'react'
 import './Square.css'
 
 function Square({ children, black }: { children?: React.ReactNode, black: boolean }) {
-  const fill = black ? 'black' : 'white';
-  const stroke = !black ? 'black' : 'white';
-
-  const style= {
-    backgroundColor: fill,
-    color: stroke
-  }
-  return <div style={{
-    position: 'absolute',
-    top: '0',
-    right: '0',
-    bottom: '0',
-    left: '0',
-    ...style }}>{children}</div>
+  const sqType=black ? 'square-black' : 'square-white';
+  return <div className={`square ${sqType}`}>{children}</div>
 }
 
 export default Square;
